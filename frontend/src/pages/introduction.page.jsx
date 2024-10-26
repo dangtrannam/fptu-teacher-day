479e53089e37c4ad388754cf7a20b60897e08d48
NhutMinhIT <nhutminh.it19@gmail.com>
2024-10-26 21:39:14 +0700

    feat: coding first page and responsivepoage

--- frontend/src/pages/introduction.page.jsx
+++ frontend/src/pages/introduction.page.jsx
@@ -1,18 +1,46 @@
+import { Link } from "react-router-dom";
 
 const IntroductionPage = ({ setNextPage }) => {
     return (
-        <div
-
-        >
-            <div className="flex flex-col justify-center items-center">
-                <button
-                    onClick={setNextPage}
-                    className="w-60 md:w-64 h-20 border rounded-full bg-gradient-to-r from-orange-600 to-orange-400 hover:from-orange-700 hover:to-orange-500 px-12"
-                >
-                    <span className="text-white text-center md:text-xl font-montserrat font-bold">
-                        Update Image
-                    </span>
-                </button>
+        <div>
+            <div className="bg-gray-400 w-full md:min-h-[14vh]">
+                <div className="flex justify-between items-center py-4 px-6 gap-4 w-full max-w-screen-xl mx-auto">
+                    <Link
+                        to="#"
+                        target="_blank"
+                    >
+                        <img
+                            src="https://via.placeholder.com/240x76"
+                            alt="fptu-logo"
+                        />
+                    </Link>
+                    <Link
+                        to="https://university.fpt.edu.vn/fptu-tuoi-18/"
+                        target="_blank"
+                    >
+                        <img
+                            src="https://via.placeholder.com/240x76"
+                            alt="Logo FTPU 18year"
+                        />
+                    </Link>
+                </div>
+            </div>
+            <div>
+                {/* Main Content Section */}
+                <div className="flex flex-col items-center justify-center px-4 py-8 space-y-6">
+                    <div className="w-full max-w-md">
+                        <img src="https://via.placeholder.com/400x240" alt="Placeholder" className="w-full h-auto" />
+                    </div>
+                    <p className="text-center max-w-md text-gray-700">
+                        Lorem ipsum dolor sit amet consectetur. Lobortis sit mauris vestibulum justo interdum.
+                    </p>
+                    <button
+                        className="px-8 py-2 bg-gray-500 text-white rounded-full"
+                        onClick={setNextPage}  // This will trigger setNextPage when clicked
+                    >
+                        BUTTON
+                    </button>
+                </div>
             </div>
         </div>
     );

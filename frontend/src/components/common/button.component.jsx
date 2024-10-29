cc9a793f1ab3778651498d204f6ea44580c13404
NhutMinhIT <nhutminh.it19@gmail.com>
2024-10-29 23:34:50 +0700

    update hover button

--- frontend/src/components/common/button.component.jsx
+++ frontend/src/components/common/button.component.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Button = ({ label, size = 'medium', variant = 'primary', onClick = () => { } }) => {
     const sizeClasses = {
-        large: 'w-60 h-14 px-6 py-3',     // kích thước lớn
-        medium: 'w-[30%] py-4',           // kích thước trung bình
-        small: 'px-8 py-4'                // kích thước nhỏ
+        large: 'w-60 h-14 px-6 py-3',
+        medium: 'w-[30%] py-4',
+        small: 'px-8 py-4'
     };
 
     const variantClasses = {
-        primary: 'bg-brand border-brand',
+        primary: 'bg-brand border-brand hover:bg-orange-600 hover:border-orange-600',
         // secondary: 'bg-gray-500 text-white border-gray-500',
         // outline: 'bg-transparent text-brand border-brand', 
-        opacity: 'bg-orange-opacity border-brand'    // nút viền
+        opacity: 'bg-orange-opacity border-brand hover:bg-brand hover:border-brand'
     };
 
     return (

5441a08d82804907312e3fab5c6acee105ddbc36
NhutMinhIT <nhutminh.it19@gmail.com>
2024-10-29 16:53:27 +0700

    fetching api tracking

--- frontend/src/service/apiConfig.js
+++ frontend/src/service/apiConfig.js
@@ -1,7 +1,9 @@
 const BASE_URL = 'http://localhost:3000';
 
 export const LOGIN_ENDPOINT = `${BASE_URL}/auth/login`;
-
 export const WISH_ENDPOINT = `${BASE_URL}/upload`;
-
 export const IMAGE_ENDPOINT = `${BASE_URL}/upload/image`;
+
+//Tracking 
+export const GET_IP_URL = 'https://api.ipify.org?format=json';
+export const TRACKING_ACCESS_ENDPOINT = `${BASE_URL}/tracking/access`;

f77f5edee86104f8aeec9fa3550a4cfb5bcb0b61
NhutMinhIT <nhutminh.it19@gmail.com>
2024-10-30 19:41:05 +0700

    api and service tracking share

--- frontend/src/service/apiConfig.js
+++ frontend/src/service/apiConfig.js
@@ -7,3 +7,4 @@ export const IMAGE_ENDPOINT = `${BASE_URL}/upload/image`;
 //Tracking 
 export const GET_IP_URL = 'https://api.ipify.org?format=json';
 export const TRACKING_ACCESS_ENDPOINT = `${BASE_URL}/tracking/access`;
+export const TRACKING_SHARE_ENDPOINT = `${BASE_URL}/tracking/share`; 

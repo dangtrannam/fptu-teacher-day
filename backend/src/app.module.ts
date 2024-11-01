6e91c525f3b8bb6c6d71461f9e9be10dd13d83b1
Nam Dang <dangtrannam2001@gmail.com>
2024-11-01 21:00:21 +0700

    add configuration for database connection and environment variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,25 @@ import { UploadModule } from './features/upload/upload.module';
 import { TrackingModule } from './features/tracking/tracking.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { AuthenticationModule } from './features/authentication/authentication.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/teacherday_app'),
+    ConfigModule.forRoot(
+      {
+        envFilePath: '.env',
+        isGlobal: true,
+      }
+    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: configService.get<string>("DATABASE_HOST"),
+        };
+      },
+      inject: [ConfigService],
+    }),
     MulterModule.register({
       dest: './files',
     }),
